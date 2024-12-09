36e6d116bb38945b5dc2655b9566a8229b4b0406
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-12-09 16:09:31 +0100

    Add components for forms

---
+++ components/modal/ModalFooter.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import ButtonInvisible from "../form/ButtonInvisible.vue";
+
+defineEmits(["finish"]);
+defineProps(["disabledButton"]);
+</script>
+
+<template>
+  <div class="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+    <ButtonInvisible
+      :disabled="disabledButton"
+      @click="$emit('finish')"
+      class="px-3"
+    >
+      {{ $t("modal.done") }}
+    </ButtonInvisible>
+  </div>
+</template>
