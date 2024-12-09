7fdf4afddd72c1b6250d148f3c3b9a9d5c1dfc71
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-12-09 16:11:52 +0100

    Add AppInfo typing

---
+++ types/index.ts
@@ -0,0 +1,9 @@
+export interface AppInfo {
+  name: string;
+  favicon: string;
+  terms_of_service: string;
+  privacy_policy: string;
+  version: number;
+  invite_only: boolean;
+  background: string;
+}
