24497ca927ce44bebfc39cecf0b4663e190c1d21
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-04-05 21:23:05 +0200

    Save cookie

--- stores/users.ts
+++ stores/users.ts
@@ -1,4 +1,11 @@
-import { mande } from "mande";
+import { isProduction } from "std-env";
+import type { ServerError, User } from "~/types";
+import { ServerErrorClass } from "~/types";
+
+interface Response {
+  user: User;
+  token: string;
+}
 
 export const useUsers = defineStore("users", {
   state: () => {
@@ -24,12 +31,38 @@ export const useUsers = defineStore("users", {
       if (opt.protocol) this.protocol = opt.protocol;
     },
 
-    async signIn(email: string, password: string, code?: string) {
+    async signIn(
+      email: string,
+      password: string,
+      totpCode?: string,
+    ): Promise<void> {
+      const route = `${this.protocol}//${this.host}/login`;
+
       try {
-        const api = mande(`${this.protocol}//${this.host}/users/login`);
-        this.userData = await api.post({ email, password, code });
+        const response = await fetch(route, {
+          method: "post",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password, totpCode }),
+        });
+
+        let json = await response.json();
+
+        if (!response.ok) {
+          json = json as ServerError;
+          throw new ServerErrorClass(json);
+        }
+
+        json = json as Response;
+        useCookie("token", {
+          maxAge: 60 * 60 * 24 * 30 * 2, // 2 months.
+          sameSite: "strict",
+          secure: isProduction,
+        }).value = json.token;
       } catch (error) {
-        return error;
+        throw error;
       }
     },
   },
