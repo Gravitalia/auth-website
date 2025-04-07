d9695bb1e9bb40f2927a91a49c3a6524c85048e7
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-04-07 13:53:20 +0200

    auto height

--- components/form/Card.vue
+++ components/form/Card.vue
@@ -6,7 +6,7 @@ const props = defineProps({
 
 <template>
   <div
-    class="bg-white dark:bg-zinc-950 w-80 lg:w-96 h-96 border border-zinc-200 dark:border-zinc-800 p-8 mt-auto flex flex-col"
+    class="bg-white dark:bg-zinc-950 w-80 lg:w-96 h-auto border border-zinc-200 dark:border-zinc-800 p-8 mt-auto flex flex-col"
   >
     <!-- Title of the card. -->
     <h1 class="text-xl font-bold mb-6">
