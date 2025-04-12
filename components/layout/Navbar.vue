8a2045af9ae5335df67da0322a5ec045a71c559c
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-04-12 21:32:51 +0200

    Dashboard layout (#7)

---
+++ components/layout/Navbar.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+const path = useRoute().path;
+const links = [
+  {
+    name: "navbar.profile",
+    path: "/",
+  },
+  {
+    name: "navbar.keys",
+    path: "/keys",
+  },
+  {
+    name: "navbar.account",
+    path: "/account",
+  },
+];
+
+const user = useUsers();
+const logout = async () => {
+  user.logout();
+  await navigateTo("/signin");
+};
+
+const { data } = await useAppInfo(`${user.host}/status.json`);
+</script>
+
+<template>
+  <nav
+    class="flex items-center justify-between bg-white px-4 md:px-24 py-3 shadow-sm rounded-t-md"
+  >
+    <div class="flex items-center space-x-10">
+      <NuxtImg v-if="data?.favicon" :src="data.favicon" class="size-10" />
+
+      <div class="flex space-x-6 text-sm font-medium">
+        <NuxtLink
+          v-for="link in links"
+          :key="link.path"
+          :to="link.path"
+          :aria-label="$t(link.name)"
+          :aria-current="link.path === path ? 'page' : null"
+          :class="[
+            'relative transition group',
+            link.path === path
+              ? 'text-black'
+              : 'text-gray-500 hover:text-black',
+          ]"
+        >
+          {{ $t(link.name) }}
+          <span
+            :class="[
+              'absolute left-0 -bottom-full h-[2px] w-full bg-black rounded-full transition-opacity',
+              link.path === path
+                ? 'opacity-100'
+                : 'opacity-0 group-hover:opacity-100',
+            ]"
+          ></span>
+        </NuxtLink>
+      </div>
+    </div>
+
+    <div class="flex items-center space-x-4">
+      <FormButtonInvisible @click="logout" class="border-none px-4">
+        {{ $t("navbar.logout") }}
+      </FormButtonInvisible>
+    </div>
+  </nav>
+</template>
--- stores/users.ts
+++ stores/users.ts
@@ -7,16 +7,30 @@ interface Response {
   token: string;
 }
 
+const TOKEN_DURATION = 60 * 60 * 24 * 30 * 2; // 2 months.
+
 export const useUsers = defineStore("users", {
   state: () => {
     let { protocol, host } = normalizeUrl(
       useRuntimeConfig().public.defaultServer,
     );
 
+    let token;
+    if (useCookie("token").value) {
+      const values = useCookie("token").value?.split(".");
+      if (values && values[0] && values[1]) {
+        host = atob(values[0]);
+        token = values[1];
+      } else if (values && values[0]) {
+        token = values[0];
+      }
+    }
+
     return {
       userData: {},
       host,
       protocol,
+      _token: token,
     };
   },
 
@@ -31,39 +45,35 @@ export const useUsers = defineStore("users", {
       if (opt.protocol) this.protocol = opt.protocol;
     },
 
+    async get(user: string = "@me"): Promise<User> {
+      const route = `${this.protocol}//${this.host}/users/${user}`;
+
+      try {
+        const response = await fetch(route, {
+          headers: {
+            Accept: "application/json",
+            Authorization: this._token ?? "",
+          },
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) throw new ServerErrorClass(data as ServerError);
+
+        return data as User;
+      } catch (error) {
+        throw error;
+      }
+    },
+
     async signIn(
       email: string,
       password: string,
       totpCode?: string,
     ): Promise<void> {
       const route = `${this.protocol}//${this.host}/login`;
-
-      try {
-        const response = await fetch(route, {
-          method: "post",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email, password, totpCode }),
-        });
-
-        let json = await response.json();
-
-        if (!response.ok) {
-          json = json as ServerError;
-          throw new ServerErrorClass(json);
-        }
-
-        json = json as Response;
-        useCookie("token", {
-          maxAge: 60 * 60 * 24 * 30 * 2, // 2 months.
-          sameSite: "strict",
-          secure: isProduction,
-        }).value = json.token;
-      } catch (error) {
-        throw error;
-      }
+      const payload = { email, password, totpCode };
+      await this._post(route, payload);
     },
 
     async signUp(
@@ -73,33 +83,49 @@ export const useUsers = defineStore("users", {
       invite?: string,
     ): Promise<void> {
       const route = `${this.protocol}//${this.host}/create`;
+      const payload = { id, email, password, invite };
+      await this._post(route, payload);
+    },
 
+    logout(): void {
+      this.userData = {};
+      this._token = undefined;
+      useCookie("token").value = undefined;
+    },
+
+    async _post(url: string, body: object): Promise<void> {
       try {
-        const response = await fetch(route, {
-          method: "post",
+        const response = await fetch(url, {
+          method: "POST",
           headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ id, email, password, invite }),
+          body: JSON.stringify(body),
         });
 
-        let json = await response.json();
+        const data = await response.json();
 
-        if (!response.ok) {
-          json = json as ServerError;
-          throw new ServerErrorClass(json);
-        }
+        if (!response.ok) throw new ServerErrorClass(data as ServerError);
 
-        json = json as Response;
-        useCookie("token", {
-          maxAge: 60 * 60 * 24 * 30 * 2, // 2 months.
-          sameSite: "strict",
-          secure: isProduction,
-        }).value = json.token;
-      } catch (error) {
-        throw error;
+        const { token } = data as Response;
+        await this._setToken(token);
+      } catch (err) {
+        throw err;
       }
+    },
+
+    async _setToken(token: string): Promise<void> {
+      this._token = token;
+
+      token = `${btoa(this.host)}.${token}`;
+      useCookie("token", {
+        maxAge: TOKEN_DURATION,
+        sameSite: "strict",
+        secure: isProduction,
+      }).value = token;
+
+      this.userData = await this.get();
     },
   },
 });
