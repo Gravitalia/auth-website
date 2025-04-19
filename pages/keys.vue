a29559890898baea9d03d97e0d5d451aa7fd5434
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-04-19 14:23:12 +0200

    Finish page

--- pages/keys.vue
+++ pages/keys.vue
@@ -1,80 +1,25 @@
 <script setup lang="ts">
+import useKeys from "~/composables/keysManager";
+
 definePageMeta({
   middleware: "auth",
   layout: "dashboard",
 });
-
-const u = useUsers();
-const user = u.userData;
-
-const deleteKey = async (keyId: string) => {
-  const id = Number(keyId);
-  await u.removeKey(id);
-  u.userData.publicKeys = u.userData.publicKeys.filter(
-    (key) => key.id !== keyId,
-  );
-};
-
-const _arrayBufferToBase64 = (buffer: any) => {
-  const bytes = new Uint8Array(buffer);
-  let binary = "";
-  bytes.forEach((byte) => {
-    binary += String.fromCharCode(byte);
-  });
-  return window.btoa(binary);
-};
-
-const generateKey = async () => {
-  const publicKeyOptions = {
-    challenge: new Uint8Array(32),
-    rp: {
-      name: "Gravitalia",
-    },
-    user: {
-      id: new Uint8Array([0]), // Not exist on Gravitalia.
-      name: user.id,
-      displayName: user.preferredUsername,
-    },
-    pubKeyCredParams: [
-      { type: "public-key", alg: -36 },
-      { type: "public-key", alg: -7 },
-      { type: "public-key", alg: -257 },
-    ],
-  };
-
-  const credential = await navigator.credentials.create({
-    publicKey: publicKeyOptions,
-  });
-  const pk = credential.response.getPublicKey();
-
-  const base64 = _arrayBufferToBase64(pk);
-  const pem =
-    `-----BEGIN PUBLIC KEY-----\n` +
-    base64.match(/.{1,64}/g).join("\n") +
-    `\n-----END PUBLIC KEY-----`;
-
-  // Upload key.
-  const id = (await u.addKey(pem))[0];
-  user.publicKeys.push({
-    id,
-    owner: user.id,
-    publicKeyPem: pem,
-    createdAt: "now",
-  });
-  useCookie("key", { sameSite: "strict", expires: undefined }).value = id;
-};
+const { keys, generateKey, deleteKey } = useKeys();
 </script>
 
 <template>
   <div class="flex flex-col justify-center items-center mt-16 gap-4">
-    <FormButtonInvisible @click="generateKey" class="w-80 lg:w-1/3"
-      >Générer une nouvelle clé</FormButtonInvisible
-    >
+    <FormButtonInvisible @click="generateKey" class="w-80 md:w-1/2 lg:w-1/3">{{
+      $t("keys.generate")
+    }}</FormButtonInvisible>
 
-    <FormCard title="Clés publiques sur le serveur" class="lg:w-1/3">
-      <div v-for="key in user.publicKeys" class="flex justify-between">
+    <FormCard :title="$t('keys.title')" class="md:w-1/2 lg:w-1/3 gap-4">
+      <div v-for="key in keys" class="flex justify-between">
         <div>
-          <p>Clé {{ key.id }} du {{ key.createdAt }}</p>
+          <p>
+            {{ $t("keys.description", { id: key.id, date: key.createdAt }) }}
+          </p>
           <NuxtLink
             :to="
               'data:application/x-pem-file;charset=utf-8,' + key.publicKeyPem
@@ -95,11 +40,11 @@ const generateKey = async () => {
                 d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
               />
             </svg>
-            Télécharger la clé publique</NuxtLink
+            {{ $t("keys.download") }}</NuxtLink
           >
         </div>
 
-        <FormButtonInvisible class="w-52" @click="deleteKey(key.id)"
+        <FormButtonInvisible class="w-32 xl:w-52" @click="deleteKey(key.id)"
           ><svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -114,7 +59,7 @@ const generateKey = async () => {
               d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
             />
           </svg>
-          <span class="mt-0.5 ml-1.5">Supprimer</span>
+          <span class="mt-0.5 ml-1.5">{{ $t("keys.delete") }}</span>
         </FormButtonInvisible>
       </div>
     </FormCard>
