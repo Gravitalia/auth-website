6f97fcd33d6ab0771b7535c47a88fa8997bcc41c
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-05-15 23:05:13 +0200

    Add toast (#10)

---
+++ components/toast/Container.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { toasts } from "~/composables/useToast";
+</script>
+
+<template>
+  <div class="fixed bottom-4 right-4 flex flex-col space-y-2 z-50">
+    <Toast
+      v-for="t in toasts"
+      :key="t.id"
+      :title="t.title"
+      :description="t.description"
+    />
+  </div>
+</template>
