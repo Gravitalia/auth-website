9ba759e0dbe14af7dd2df3d232ac4daf3f293870
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-05-16 11:48:04 +0200

    Add example to generate client-side challenge

---
+++ pages/example_challenge_generation.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const b64Challenge = ref("");
+onMounted(() => {
+  const generateChallenge = (length = 32) => {
+    const challenge = new Uint8Array(length);
+    window.crypto.getRandomValues(challenge);
+    return challenge;
+  };
+
+  const challenge = generateChallenge();
+  b64Challenge.value = btoa(String.fromCharCode(...challenge));
+});
+</script>
+
+<template>
+  <p>Example of base64 encoded challenge: {{ b64Challenge }}</p>
+</template>
