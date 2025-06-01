3eb959c7e1efab554c7f65468ddf2184eb235054
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-06-01 22:46:17 +0200

    Add QR Code image for TOTP (#14)

---
+++ components/qrcode/QRCode.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import QRCode from "qrcode";
+
+const { dataUri } = defineProps({
+  dataUri: {
+    type: String,
+    default: "",
+  },
+});
+const src = ref();
+
+const generateQRCode = async (text: string) => {
+  try {
+    src.value = await QRCode.toDataURL(text);
+  } catch (_) {}
+};
+
+watch(
+  () => dataUri,
+  (value) => {
+    generateQRCode(value);
+  },
+  { immediate: true },
+);
+</script>
+
+<template>
+  <NuxtImg :src class="size-40" />
+</template>
