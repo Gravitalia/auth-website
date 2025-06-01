cacdefedf0fe1f642f49252114c425bc9a7f0f48
Hinome <57831472+RealHinome@users.noreply.github.com>
2025-06-01 18:50:47 +0200

    Fix missing emit close

--- components/modal/ModalHeader.vue
+++ components/modal/ModalHeader.vue
@@ -8,7 +8,7 @@ defineProps(["title", "description"]);
 <template>
   <div class="flex justify-between">
     <h3 class="text-xl font-semibold">{{ title }}</h3>
-    <XMarkIcon class="mt-1.5 size-4 cursor-pointer" />
+    <XMarkIcon @click="$emit('close')" class="mt-1.5 size-4 cursor-pointer" />
   </div>
   <p class="mt-2 text-sm text-zinc-600 dark:text-zinc-300">
     {{ description }}
