7bd1c47a240b6786d813c7d2b074c040e4cc53d7
RealHinome <acn@gravitalia.com>
2025-11-21 19:52:50 +0100

    Improve default background

--- components/authentification/Background.vue
+++ components/authentification/Background.vue
@@ -5,7 +5,12 @@ const props = defineProps({
 </script>
 
 <template>
-    <svg class="absolute -z-10" width="100%" height="100%" preserveAspectRatio="xMidYMid slice">
+	<svg
+		class="absolute -z-10"
+		width="100%"
+		height="100%"
+		preserveAspectRatio="xMidYMid slice"
+	>
 		<image class="w-screen h-screen" :href="props.href" />
 	</svg>
 </template>
